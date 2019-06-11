0ca583d5af98c31c6f20847835d6c0e484a8f659
baimingyu <1499714737@qq.com>
2019-06-11 23:37:32 +0800

    删除

--- SRC/COM/白/测试/TestGit.java
+++ SRC/COM/白/测试/TestGit.java
@@ -4,14 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		
-		System.out.println("aaa1143341111");
-			
-		System.out.println("22222222");
-		System.out.println("33333333");
-		System.out.println("22222222");
-		System.out.println("33333333");
-		System.out.println("22222222");
-		System.out.println("33333333");
+	
 		System.out.println("五组练习用git项目");
 		System.out.println("景清晨跳芭蕾");
 		System.out.println("小盖同学到此一游");
