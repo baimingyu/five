948b49fa92db7f98588d8f5e07a4bb0c4650d915
jingqingchen <1559454790@qq.com>
2019-06-11 21:33:05 +0800

    jingqingchen

--- SRC/COM/白/测试/TestGit.java
+++ SRC/COM/白/测试/TestGit.java
@@ -11,7 +11,8 @@ public class TestGit {
 		System.out.println("五组练习用git项目");
 		System.out.println("景清晨跳芭蕾");
 		System.out.println("小盖同学到此一游");
-		System.out.printin("景清晨跳芭蕾，晓东在旁边伴舞");
+
+		System.out.printin("1111景清晨跳芭蕾，晓东在旁边伴舞");
 			System.out.printin("景清晨跳芭蕾，晓东在旁边伴舞fafdfdf");
 		
 	}
