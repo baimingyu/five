df912a8f78ea744d530a72da3bdd40d8b7637510
baimingyu <1499714737@qq.com>
2019-06-11 23:23:35 +0800

    test

--- SRC/COM/白/测试/TestGit.java
+++ SRC/COM/白/测试/TestGit.java
@@ -4,8 +4,8 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		
-		System.out.println("1143341111");
-
+		System.out.println("aaa1143341111");
+			
 		System.out.println("22222222");
 		System.out.println("33333333");
 		System.out.println("五组练习用git项目");
