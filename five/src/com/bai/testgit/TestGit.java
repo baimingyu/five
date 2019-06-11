34c5602f2009ba332e35c9554c76e992410aac48
baimingyu <1499714737@qq.com>
2019-06-11 19:15:31 +0800

    第五组练习修改1-白明宇

---
+++ five/src/com/bai/testgit/TestGit.java
@@ -0,0 +1,13 @@
+package com.bai.testgit;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("11111111");
+		System.out.println("22222222");
+		System.out.println("123456789");
+		System.out.println("五组练习用git项目");
+
+	}
+}
