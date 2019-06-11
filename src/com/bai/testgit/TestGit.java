db2ca2f8e9ba6f1d5c7e19637d5823fad62a588d
duanxiaodong <429770775@qq.com>
2019-06-11 20:35:19 +0800

    太想念了

--- src/com/bai/testgit/TestGit.java
+++ src/com/bai/testgit/TestGit.java
@@ -7,6 +7,8 @@ public class TestGit {
 		System.out.println("11111111");
 		System.out.println("22222222");
 		System.out.println("33333333");
+		System.out.println("这其实是一个悲伤的故事");
+		System.out.println("我就静静的不说话");
 		System.out.println("五组练习用git项目");
 	}
 }
